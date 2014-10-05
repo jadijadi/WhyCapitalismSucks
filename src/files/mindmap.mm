e0649aea3449270bebf26f9d5a77927835e473eb
jadijadi <jadijadi@gmail.com>
2014-10-05 15:35:47 +0330

    end of the first day. we have the structure and the ABOUT

---
+++ src/files/mindmap.mm
@@ -0,0 +1,95 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1412496600432" ID="ID_20625899" MODIFIED="1412498360621" TEXT="&#x6a9;&#x62a;&#x627;&#x628;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1412496672985" ID="ID_275494977" MODIFIED="1412497841173" POSITION="right" TEXT="&#x645;&#x642;&#x62f;&#x645;&#x647; - &#x686;&#x631;&#x627; &#x627;&#x6cc;&#x646; &#x6a9;&#x62a;&#x627;&#x628; &#x648; &#x686;&#x637;&#x648;&#x631;&#x6cc;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1412496709591" ID="ID_223127678" MODIFIED="1412497801438" TEXT="&#x647;&#x6a9;&#x627;&#x62a;&#x648;&#x646; &#x648; &#x646;&#x648;&#x634;&#x62a;&#x646; &#x6a9;&#x62a;&#x627;&#x628;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412496714926" ID="ID_776216237" MODIFIED="1412497801437" TEXT="&#x627;&#x646;&#x62a;&#x62e;&#x627;&#x628; &#x633;&#x637;&#x62d; &#x648; &#x634;&#x6a9;&#x644; &#x628;&#x62d;&#x62b;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1412497251518" ID="ID_751252406" MODIFIED="1412498170299" POSITION="right" TEXT="&#x62a;&#x648;&#x635;&#x6cc;&#x641; &#x648;&#x636;&#x639;&#x6cc;&#x62a;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1412498026996" ID="ID_1894903478" MODIFIED="1412498033467" TEXT="&#x628;&#x627;&#x632;&#x62a;&#x648;&#x644;&#x6cc;&#x62f; - &#x641;&#x642;&#x631; &#x648; &#x641;&#x631;&#x647;&#x646;&#x6af; "/>
+<node CREATED="1412498374655" ID="ID_498399254" MODIFIED="1412498375963" TEXT="&#x634;&#x62e;&#x635;&#x6cc; &#x646;&#x6cc;&#x633;&#x62a;"/>
+</node>
+<node CREATED="1412496816391" ID="ID_776646534" MODIFIED="1412498464506" POSITION="right" TEXT="&#x622;&#x644;&#x62a;&#x631;&#x646;&#x627;&#x62a;&#x6cc;&#x648;&#x647;&#x627;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1412496827882" ID="ID_439099079" MODIFIED="1412497801432" TEXT="&#x62c;&#x62f;&#x627; &#x6a9;&#x631;&#x62f;&#x646; &#x632;&#x646;&#x62f;&#x6af;&#x6cc; &#x634;&#x62e;&#x635;&#x6cc;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412496831606" ID="ID_1026963832" MODIFIED="1412497801431" TEXT="&#x627;&#x644;&#x62a;&#x631;&#x646;&#x627;&#x62a;&#x6cc;&#x648;&#x647;&#x627;&#x6cc; &#x633;&#x6cc;&#x627;&#x633;&#x6cc;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497509816" ID="ID_1139023976" MODIFIED="1412497801431" TEXT="&#x627;&#x632;&#x644;&#x6cc; &#x648; &#x627;&#x628;&#x62f;&#x6cc; &#x646;&#x6cc;&#x633;&#x62a;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497597229" ID="ID_1585783809" MODIFIED="1412497801430" TEXT="&#x647;&#x641;&#x62a;&#x627;&#x62f; &#x633;&#x627;&#x644;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497696299" ID="ID_1980246247" MODIFIED="1412497801430" TEXT="&#x62c;&#x646;&#x628;&#x647; &#x647;&#x627;&#x6cc; &#x645;&#x62b;&#x628;&#x62a; &#x62f;&#x646;&#x6cc;&#x627; &#x627;&#x632; &#x6a9;&#x62c;&#x627; &#x627;&#x648;&#x645;&#x62f;&#x647;&#x61f;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497452334" ID="ID_340705849" MODIFIED="1412498316593" TEXT="&#x622;&#x6cc;&#x627; &#x647;&#x645;&#x647; &#x62c;&#x627; &#x627;&#x6cc;&#x646;&#x637;&#x648;&#x631;&#x6cc; &#x627;&#x633;&#x62a;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1412497553422" ID="ID_1774759640" MODIFIED="1412497801424" TEXT="&#x62c;&#x645;&#x647;&#x648;&#x631;&#x6cc; &#x627;&#x633;&#x644;&#x627;&#x645;&#x6cc; &#x647;&#x645; &#x633;&#x631;&#x645;&#x627;&#x6cc;&#x647; &#x62f;&#x627;&#x631;&#x6cc; &#x627;&#x633;&#x62a;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1412498226531" ID="ID_655365153" MODIFIED="1412498357165" POSITION="left" TEXT="&#x686;&#x631;&#x627;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1412496690048" ID="ID_1861267907" MODIFIED="1412498248397" TEXT="&#x67e;&#x648;&#x644; - &#x686;&#x637;&#x648;&#x631;&#x6cc; &#x632;&#x646;&#x62f;&#x6af;&#x6cc; &#x631;&#x648; &#x62e;&#x631;&#x627;&#x628; &#x645;&#x6cc; &#x6a9;&#x646;&#x647;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412496868997" ID="ID_368589235" MODIFIED="1412497801436" TEXT="&#x631;&#x642;&#x627;&#x628;&#x62a; &#x648; &#x645;&#x633;&#x627;&#x628;&#x642;&#x647; &#x67e;&#x6cc;&#x634;&#x631;&#x641;&#x62a;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412496887253" ID="ID_1481403953" MODIFIED="1412497801435" TEXT="&#x6a9;&#x627;&#x631;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1412497152863" ID="ID_1659898759" MODIFIED="1412497801435" TEXT="&#x6a9;&#x648;&#x62f;&#x6a9;&#x627;&#x646;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497156541" ID="ID_297672141" MODIFIED="1412497801434" TEXT="&#x647;&#x645;&#x647; &#x627;&#x632; &#x6a9;&#x627;&#x631;&#x647;&#x627; &#x645;&#x62a;&#x646;&#x641;&#x631;&#x6cc;&#x645;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1412496909790" ID="ID_1541344656" MODIFIED="1412497801433" TEXT="&#x62f;&#x648;&#x633;&#x62a;&#x6cc; &#x648; &#x631;&#x648;&#x627;&#x628;&#x637; &#x627;&#x62c;&#x62a;&#x645;&#x627;&#x639;&#x6cc;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497464473" ID="ID_787375249" MODIFIED="1412498297119" TEXT="&#x6a9;&#x644;&#x6cc;&#x634;&#x647; &#x647;&#x627;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497003813" ID="ID_511646093" MODIFIED="1412497801428" TEXT="&#x645;&#x646;&#x627;&#x641;&#x639; &#x633;&#x631;&#x645;&#x627;&#x6cc;&#x647; &#x62f;&#x627;&#x631;&#x6cc; &#x62f;&#x631; &#x62a;&#x628;&#x639;&#x6cc;&#x636; &#x647;&#x627;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497031851" ID="ID_1440648523" MODIFIED="1412497801428" TEXT="&#x631;&#x633;&#x627;&#x646;&#x647;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497046036" ID="ID_631106544" MODIFIED="1412497801427" TEXT="&#x645;&#x635;&#x631;&#x641;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497047793" ID="ID_1326623160" MODIFIED="1412497801427" TEXT="&#x645;&#x62d;&#x6cc;&#x637; &#x632;&#x6cc;&#x633;&#x62a;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497084673" ID="ID_1166378229" MODIFIED="1412497801427" TEXT="&#x628;&#x6cc;&#x6af;&#x627;&#x646;&#x6af;&#x6cc;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497133260" ID="ID_117278383" MODIFIED="1412497801426" TEXT="&#x62c;&#x646;&#x6af;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412497255348" ID="ID_1631306910" MODIFIED="1412497801426" TEXT="&#x634;&#x627;&#x62f;&#x6cc;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1412496948203" ID="ID_952017469" MODIFIED="1412497801429" POSITION="right" TEXT="&#x645;&#x646;&#x627;&#x628;&#x639; &#x628;&#x631;&#x627;&#x6cc; &#x645;&#x637;&#x627;&#x644;&#x639;&#x647; &#x628;&#x6cc;&#x634;&#x62a;&#x631;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1412496953663" ID="ID_498281785" MODIFIED="1412497801429" POSITION="right" TEXT="&#x647;&#x645;&#x6a9;&#x627;&#x631;&#x6cc;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
--- src/partials/footer.html.md
+++ src/partials/footer.html.md
@@ -1,9 +1,7 @@
 
   <div class="well">
 	<p class="footer">
-		کتاب لینوکس و زندگی کاری است از 
-		<a href="http://jadi.net">جادی</a> روی 
-		<a href="https://github.com/jadijadi/justforfun">گیت هاب</a>
+این یک کتاب آزاد است. آن را [پخش کنید](/download.html)، [توسعه دهید](/chapters/contribute.html) و از آن [حمایت کنید](/support.html).
 		<br />
 		<a href=/>روی جلد</a> | <a href="/support.html">حمایت</a> | <a href="/about.html">درباره</a>
 	</p>
